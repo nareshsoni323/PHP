eeecf1b91a48136266dfdc8ce3d833c9979cf150
Kiều Thị Ánh <anhkt1603@gmail.com>
2016-09-12 11:10:14 +0700

    My web simple

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+</head>
+<body>
+
+<div id="container">
+  <div id="menu">
+  <ul>
+    <li><a href ="#">Trang chủ</a></li>
+    <li><a href ="#">About me</a></li>
+    <li><a href ="#">Công nghê</a></li>
+    <li><a href ="#">Ruby</a></li>
+    <li><a href="#"> JavaScript</a></li>
+    <li><a href ="#">PHP</a>
+    	<ul class="sub-menu">
+        <li><a href="#"> Laravel</a></li>
+        <li><a href="#"> WordPress</a></li>
+        <li><a href="#"> MVC</a></li>
+         <li><a href="#">OOP</a></li>
+      </ul>
+    </li>
+  </ul>
+  </div>
+  <div id="content">
+    <div id="header">
+      <div id="logo"><img src="">Logo</div>
+      <div id="banner"><img src="">Banner</div>
+    </div>
+    <div class="call-to-action">
+      <h3>Hôm nay thi max chán luôn. Haizz</h3>
+      <p>Thi xong rồi, cố gắng học cái này thôi</p>
+    </div>
+    <div class="row">
+      <div id="box1" class="col">
+        <h2>PHP là gì?</h2>
+        <img src="">
+        <p>PHP là ngôn ngữ lập trình, code chức năng cho các trang web</p>
+      </div>
+      <div id="box2" class="col">
+        <h2>HTML là gì?</h2>
+         <p>HTML là ngôn ngữ đánh dấu siêu văn bản,giúp chúng ta xây dựng khung trang web</p>
+      </div>
+      <div id="box3" class="col">
+         <h2>CSS là gì?</h2>
+         <p>CSS là công cụ để chúng ta tạo nên màu sắc và vẻ đẹp cho trang web</p>
+      </div>
+    </div>
+  </div>
+  <div id="footer">
+   <p>Copyright &copy; 2016 - Cunghocvoitoi.com</p>
+  </div>
+</div>
+</body>
+</html>
